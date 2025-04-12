f6cae05690f12c25a0a517820836ced164dc4fbc
Rithwik Srivastava <rithwiksrivastava@MacBook-Pro.local>
2025-04-11 22:53:23 -0400

    Final changes

--- routes/healthz.js
+++ routes/healthz.js
@@ -4,7 +4,6 @@ const { performHealthCheck } = require('../controllers/healthzController');
 
 // This will handle requests to /healthz because of how we mounted the router in server.js
 router.get('/healthz',performHealthCheck);
-router.get('/cicd',performHealthCheck);
 
 // Handle unsupported HTTP methods for health check
 router.all('/healthz', (req, res) => {
@@ -12,9 +11,4 @@ router.all('/healthz', (req, res) => {
     res.status(405).send();
 });
 
-// Handle unsupported HTTP methods for health check
-router.all('/cicd', (req, res) => {
-    // Method Not Allowed
-    res.status(405).send();
-});
 module.exports = router;
